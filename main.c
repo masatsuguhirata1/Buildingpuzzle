702ed8b869870b9e1deabc23ec85160b82bc56e1
Masatsugu Hirata <mhirata@c1r21s4.42tokyo.jp>
2020-01-18 13:27:23 +0900

    init

---
+++ main.c
@@ -0,0 +1,22 @@
+
+void display(int *buildingpuzzle)
+{
+	while (i )
+
+}
+
+
+int main()
+{
+	col1up=
+	int i=0,j=0;
+	int size = 4;
+
+	int buildingpuzzle[size][size];
+
+
+	while (i < :size){
+		while (j < size){
+			display();
+	}
+}
